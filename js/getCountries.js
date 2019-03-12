7500a18c8da359173c93ab3056fabbec0af9667c
Viktoria Yordanova <yordanova98@abv.bg>
2019-03-12 22:52:22 +0200

    some commit

--- js/getCountries.js
+++ js/getCountries.js
@@ -82,4 +82,62 @@ document.getElementById("getCountries").addEventListener("click", function(){
     else{
         alert('You already clicked it');
     }
+    const Group = (function () {
+        let teams;
+        const render = async function() {
+            const teamToHtml = function (team) {
+                const result = 
+                `
+                    <tr id="${team.fifa_code}">
+                        <td>${team.country}</td>
+                        <td>${team.fifa_code}</td>
+                        <td>${team.wins}</td>
+                        <td>${team.draws}</td>
+                        <td>${team.losses}</td>
+                        <td>${team.points}</td>
+                    </tr>
+                `;
+                return result;
+            }
+            const group = HashService.getParts()[1];
+            if(!group) {
+                return "Group not found!";
+            }
+            const teams = await GroupService.getTeams(group);
+            if (teams.length === 0) {
+                return "Group not found!";
+            }
+            const result = 
+            `
+                <h1 style="text-align: center">Group ${group.toUpperCase()}</h1>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <td>Country</td>
+                            <td>Fifa Code</td>
+                            <td>Group number</td>
+                            <td>Group letter</td>
+                            <td>Number</td>
+                            <td>Points</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${teams
+                            .map(teamToHtml)
+                            .join('\n')}
+                    </tbody>
+                </table>
+            `;
+            return result;
+        };
+    
+        const after_render = async function () {
+    
+        }
+    
+        return {
+            render,
+            after_render
+        }
+    })();
 });
